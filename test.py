59424db756de22a7cf2df6aea5cea3dbffa86ddc
gliverneaux <40613258+gliverneaux@users.noreply.github.com>
2022-04-15 19:06:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-def main():
+import os
+
+def main(argv):
     print("Hello World!")
-    secret=12345
+    secret="12345"
+    password="4543"
+    startdir = os.path.abspath(os.curdir)
+    requested_path = os.path.relpath(argv, startdir)
+    print(requested_path)
+    tfile = open(requested_path, 'rb')
     
     
 if __name__ == "__main__":
-  main()
+    main(sys.argv[1:])
